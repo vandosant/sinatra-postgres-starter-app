4914f9deffce88530cb077a1e34a8e4f7b4371b5
vandosant <sjskender@gmail.com>
2014-04-27 17:31:30 -0600

    Setup databases for dev and test, added artists table

--- scripts/create_databases.sql
+++ scripts/create_databases.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE <your development database name>
-WITH OWNER = <your local db user>;
+CREATE DATABASE sinatra_postgres_development
+WITH OWNER = gschool_user;
 
-CREATE DATABASE <your test database name>
-WITH OWNER = <your local db user>;
+CREATE DATABASE sinatra_postgres_test
+WITH OWNER = gschool_user;
